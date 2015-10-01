b0467a9ab76f134f4b28189f56627f9456c45a60
kolomiytsev <leonid.kolomiytsev@gmail.com>
2015-10-01 16:55:37 +0300

    Singleton implementation and hasOwnPropery usage fixed. One more example added.

--- validator.js
+++ validator.js
@@ -1,18 +1,15 @@
 var validationSchema = require('./scheme'),
-    userModel = [];
-module.exports = new ObjectValidator();
-
-//constructor for object validator
-function ObjectValidator() {
-    this.registerModel = function (name, model) {
-        if (userModel.hasOwnProperty(name)) {
+    userModel = [],
+    ObjectValidator = module.exports = {
+    registerModel: function (name, model) {
+        if (!!userModel[name]) {
             console.log('Model ' + name + ' is already in the system.');
             return false;
         }
         var schema = [];
         for (var prop in model) {
             var type = model[prop].type;
-            if (!validationSchema.hasOwnProperty(type)) {
+            if (!validationSchema[type]) {
                 console.log('validationSchema does not have ' + type +
                 ' type, please update validationSchema or register new Model');
                 return false;
@@ -20,8 +17,8 @@ function ObjectValidator() {
             schema[prop] = validationSchema[type].validate(model[prop]);
         }
         userModel[name] = schema;
-    };
-    this.validate = function (name, object) {
+    },
+    validate: function (name, object) {
         if (!userModel[name]) {
             console.log(name + ' is not registered!');
             return false;
@@ -35,25 +32,37 @@ function ObjectValidator() {
             }
         }
         return true;
-    };
-    this.dispose = function () {
+    },
+    dispose: function () {
         userModel = [];
-    };
-}
+    }
+};
 
 //Example
-//var test = new ObjectValidator();
-//test.registerModel("user", {
+//ObjectValidator.registerModel("user", {
 //    id: {type: "uuid", required: true},
 //    name: {type: "string", min: 1, max: 64},
 //    createdAt: {type: "date"},
 //    counter: {type: "number", required: true, min: 0, max: 64}
 //});
-//var result = test.validate("user", {
+//ObjectValidator.registerModel("user2", {
+//    id: {type: "uuid", required: true},
+//    name: {type: "string", min: 1, max: 6},
+//    createdAt: {type: "date"},
+//    counter: {type: "number", required: true, min: 0, max: 64}
+//});
+//var result = ObjectValidator.validate("user", {
 //    id: "030cbcef-8308-48ee-9ce5-5b305c36f154",
 //    name: "Leonid Kolomiytsev",
 //    createdAt: new Date(),
 //    counter: 0
 //});
+//var result2 = ObjectValidator.validate("user2", {
+//    id: "030cbcef-8308-48ee-9ce5-5b305c36f154",
+//    name: "Kolomiytsev",
+//    createdAt: new Date(),
+//    counter: 0
+//});
 //
 //console.log(result);
+//console.log(result2);
